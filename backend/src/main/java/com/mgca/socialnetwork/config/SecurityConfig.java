86493b359994a34409971a2a091cb483fba96301
Eduardomelomg <eduardomelomg0704@gmail.com>
2026-06-19 16:48:25 -0300

    auth repair

--- backend/src/main/java/com/mgca/socialnetwork/config/SecurityConfig.java
+++ backend/src/main/java/com/mgca/socialnetwork/config/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(org.springframework.security.config.Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
